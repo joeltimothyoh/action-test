6f7c93feb18dd638f132ca5a1f8976cecf7c607c
Joel Timothy Oh <joeltimothyoh@live.com>
2019-10-21 08:58:16 +0800

    Initial commit

---
+++ entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh -e
+
+echo welcome
+echo
+echo first name: $1
+echo middle name: $2
+echo last name: $3
+echo arg4: $4
+echo env_var1: $env_var1
+
+echo system info:
+hostname
+whoami
+
+echo goodbye!
